44632580a6e8545ef165a254955b9d21fb47976e
Tony Walmsley <tony@aosd.co.uk>
2021-05-04 09:31:48 +0100

    Export the NumCLient interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { createClient, DefaultCallbackHandler, NumProtocolErrorCode } from './client';
+import { createClient, DefaultCallbackHandler, NumClient, NumProtocolErrorCode } from './client';
 import { createDnsClient, DoHResolver } from './dnsclient';
 import { buildNumUri, Hostname, NumUri, parseNumUri, PositiveInteger, UrlPath, UrlUserInfo } from './numuri';
 import { resolvePath } from './urlrelativepathresolver';
 
-export { parseNumUri, NumUri, PositiveInteger, Hostname, UrlUserInfo, UrlPath, buildNumUri };
+export { NumClient, parseNumUri, NumUri, PositiveInteger, Hostname, UrlUserInfo, UrlPath, buildNumUri };
 export { createClient, NumProtocolErrorCode, DefaultCallbackHandler };
 export { DoHResolver, createDnsClient };
 export { resolvePath };
