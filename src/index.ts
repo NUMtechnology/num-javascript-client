debbdd2cc13aba3ad480b8dab6fadf907359786c
Tony Walmsley <tony@aosd.co.uk>
2021-05-04 11:35:12 +0100

    Export MODULE_x constants

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
+/* eslint-disable max-len */
 import { createClient, DefaultCallbackHandler, NumClient, NumProtocolErrorCode } from './client';
 import { createDnsClient, DoHResolver } from './dnsclient';
-import { buildNumUri, Hostname, NumUri, parseNumUri, PositiveInteger, UrlPath, UrlUserInfo } from './numuri';
+import {
+  buildNumUri,
+  Hostname,
+  MODULE_0,
+  MODULE_1,
+  MODULE_10,
+  MODULE_2,
+  MODULE_3,
+  MODULE_4,
+  MODULE_5,
+  MODULE_6,
+  MODULE_7,
+  MODULE_8,
+  MODULE_9,
+  NumUri,
+  parseNumUri,
+  PositiveInteger,
+  UrlPath,
+  UrlUserInfo,
+} from './numuri';
 import { resolvePath } from './urlrelativepathresolver';
 
 export { NumClient, parseNumUri, NumUri, PositiveInteger, Hostname, UrlUserInfo, UrlPath, buildNumUri };
+export { MODULE_0 };
+export { MODULE_1 };
+export { MODULE_2 };
+export { MODULE_3 };
+export { MODULE_4 };
+export { MODULE_5 };
+export { MODULE_6 };
+export { MODULE_7 };
+export { MODULE_8 };
+export { MODULE_9 };
+export { MODULE_10 };
 export { createClient, NumProtocolErrorCode, DefaultCallbackHandler };
 export { DoHResolver, createDnsClient };
 export { resolvePath };
